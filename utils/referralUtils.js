1c25f76067e600ddd45105ecd13e2b295080fcde
binarybodhi <neohex262@gmail.com>
2024-10-24 15:13:22 +0400

    Update referralUtils.js

--- utils/referralUtils.js
+++ utils/referralUtils.js
@@ -1,3 +1,4 @@
+
 const crypto = require('crypto');
 
 const generateReferralCode = () => {
@@ -5,10 +6,21 @@ const generateReferralCode = () => {
 };
 
 const validateReferralCode = (code) => {
-  return typeof code === 'string' && code.length === 8 && /^[0-9A-F]{8}$/i.test(code);
+  if (!code || typeof code !== 'string') return false;
+  return code.length === 8 && /^[0-9A-F]{8}$/i.test(code);
 };
 
 const calculateReferralReward = (level, xp) => {
+  if (typeof level !== 'number' || typeof xp !== 'number') {
+    throw new Error('Invalid parameters for reward calculation');
+  }
+  if (level < 1 || level > 3) {
+    throw new Error('Invalid referral level');
+  }
+  if (xp < 0) {
+    throw new Error('Invalid XP amount');
+  }
+  
   const percentages = [0.1, 0.05, 0.025];
   return Math.floor(xp * (percentages[level - 1] || 0));
 };
